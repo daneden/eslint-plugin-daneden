be73db1e47c959af40a4bbf6b3a3a39453a75d6c
Daniel Eden <dan.eden@me.com>
2020-07-05 14:08:04 +0100

    Remove top-level directive

--- index.js
+++ index.js
@@ -2,18 +2,4 @@ const reqAll = require("import-modules")
 
 module.exports = {
   rules: reqAll("rules", { camelize: false }),
-  configs: {
-    recommended: {
-      env: {
-        es6: true,
-      },
-      parserOptions: {
-        ecmaVersion: 2016,
-        sourceType: "module",
-      },
-      rules: {
-        "explicit-let-type": "error",
-      },
-    },
-  },
 }
